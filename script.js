91eff7e5cc264cc20c84b5ec607f28c8f0288a9e
Everton Rocha <irocha10@hotmail.com>
2022-04-30 01:34:58 -0300

    Geolocation API

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+//TEST IF EXISTS NAVIGATOR OBJECT
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (pos) {
+      const { latitude: lati, longitude: long } = pos.coords;
+      console.log(`https://www.google.com.br/maps/@${lati},${long}`);
+    },
+    function () {
+      alert('errorr');
+    }
+  );
+}
